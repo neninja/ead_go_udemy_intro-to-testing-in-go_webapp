a2e9e708a2a49839867e632787b709bfcfac9827
Neni <felipe.dacs@gmail.com>
2023-10-01 16:26:42 -0300

    :tv: add aula 4

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,24 +3,33 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"path"
 )
 
+var pathToTemplates = "./templates/"
+
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-  _ = app.render(w, r, "home.page.tpl", &TemplateData{})
+	_ = app.render(w, r, "home.page.tpl", &TemplateData{})
 }
 
 type TemplateData struct {
-  IP string
-  Date map[string]any
+	IP   string
+	Data map[string]any
 }
 
-func (app *application) render(w http.ResponseWriter, r *http.Request, t string, td *TemplateData) error {
-  parsedTemplate, err := template.ParseFiles("./templates/" + t)
-  if err != nil {
-    http.Error(w, "bad request", http.StatusBadRequest)
-  }
+func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
+	// parse the template from disk.
+	parsedTemplate, err := template.ParseFiles(path.Join(pathToTemplates, t))
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
 
-  err = parsedTemplate.Execute(w, td)
-  
-  return nil
+	// execute the template, passing it data, if any
+	err = parsedTemplate.Execute(w, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
