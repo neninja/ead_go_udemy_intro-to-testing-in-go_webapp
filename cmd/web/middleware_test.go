edfd2cefaa7bb6cb4347211f42043c77c81f5c96
Neni <felipe.dacs@gmail.com>
2023-10-07 13:54:24 -0300

    :tv: add aula 7

---
+++ cmd/web/middleware_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var app application
+
+func Test_application(t *testing.T) {
+	var tt = []struct {
+		headerName  string
+		headerValue string
+		addr        string
+		emptyAddr   bool
+	}{
+		{"", "", "", false},
+		{"", "", "", true},
+		{"X-Forwarded-For", "192.3.2.1", "", false},
+		{"", "", "hello:world", false},
+	}
+
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		val := r.Context().Value(contextUserKey)
+        if val == nil {
+            t.Error(contextUserKey, "not present")
+        }
+
+        ip, ok := val.(string)
+        if !ok {
+            t.Error("not string")
+        }
+
+        t.Log(ip)
+	})
+
+    for _, e := range tt {
+        handlerToTest := app.addIPToContext(nextHandler)
+
+        req := httptest.NewRequest("GET", "http://testing", nil)
+
+        if e.emptyAddr {
+            req.RemoteAddr = ""
+        }
+
+        if len(e.headerName) > 0 {
+            req.Header.Add(e.headerName, e.headerValue)
+        }
+
+        if len(e.addr) > 0 {
+            req.RemoteAddr = e.addr
+        }
+
+        handlerToTest.ServeHTTP(httptest.NewRecorder(), req)
+    }
+}
