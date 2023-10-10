98be3584dd75fab35c830aa66485e7616ff24b9f
Neni <felipe.dacs@gmail.com>
2023-10-09 21:52:07 -0300

    :tv: add aula 19

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,12 +5,22 @@ import (
 	"html/template"
 	"net/http"
 	"path"
+	"time"
 )
 
 var pathToTemplates = "./templates/"
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	_ = app.render(w, r, "home.page.tpl", &TemplateData{})
+    td := make(map[string]any)
+
+    if app.Session.Exists(r.Context(), "test") {
+        msg := app.Session.GetString(r.Context(), "test")
+        td["test"] = msg
+    } else {
+        app.Session.Put(r.Context(), "test", "Hit this page at " + time.November.String())
+    }
+
+    _ = app.render(w, r, "home.page.tpl", &TemplateData{Data: td})
 }
 
 type TemplateData struct {
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,16 +15,13 @@ func main() {
 	// setup
 	app := application{}
 
-	// routes
-	mux := app.routes()
-
     app.Session = getSession()
 
 	// print out a message
 	log.Println("Starting server on port 8000...")
 
 	// start server
-	err := http.ListenAndServe(":8000", mux)
+	err := http.ListenAndServe(":8000", app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
