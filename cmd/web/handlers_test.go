c2d54b23da647c5333aaa80d41db4bcb98e120fe
Neni <felipe.dacs@gmail.com>
2023-10-11 00:34:58 -0300

    :tv: add aula 22

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -25,8 +25,6 @@ func Test_application_handlers(t *testing.T) {
 	ts := httptest.NewTLSServer(routes)
 	defer ts.Close()
 
-	pathToTemplates = "./../../templates/"
-
 	// range through test data
 	for _, e := range theTests {
 		resp, err := ts.Client().Get(ts.URL + e.url)
@@ -38,27 +36,6 @@ func Test_application_handlers(t *testing.T) {
 		if resp.StatusCode != e.expectedStatusCode {
 			t.Errorf("for %s: expected status %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 		}
-	}
-}
-
-func TestAppHomeOld(t *testing.T) {
-	req, _ := http.NewRequest("GET", "/", nil)
-
-	req = addContextAndSessionToRequest(req, app)
-
-	rr := httptest.NewRecorder()
-
-	handler := http.HandlerFunc(app.Home)
-
-	handler.ServeHTTP(rr, req)
-
-	if rr.Code != http.StatusOK {
-		t.Errorf("TestAppHome returned wrong status code; expected 200 and got %d", rr.Code)
-	}
-
-	body, _ := io.ReadAll(rr.Body)
-	if !strings.Contains(string(body), `<small>From Session`) {
-		t.Error("did not find correct text in html")
 	}
 }
 
@@ -96,6 +73,19 @@ func TestAppHome(t *testing.T) {
 		if !strings.Contains(string(body), e.expectedHTML) {
 			t.Errorf("%s: did not find %s correct text in html", e.name, e.expectedHTML)
 		}
+	}
+}
+
+func TestApp_renderWithBadTemplate(t *testing.T) {
+	pathToTemplates = "./testdata"
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	req = addContextAndSessionToRequest(req, app)
+	rr := httptest.NewRecorder()
+
+	err := app.render(rr, req, "bad.page.tpl", &TemplateData{})
+	if err == nil {
+		t.Error("expected error from bad template, but did not get one")
 	}
 }
 
---
+++ cmd/web/testdata/bad.page.tpl
@@ -0,0 +1,26 @@
+<!-- vim: set ft=html: -->
+{{template "base" .}}
+
+{{define "content"}}
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="mt-3">Home page</h1>
+            <form action="login" method="post">
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+
+            <hr/>
+            <small>{{$nonExistentVar}}</small><br/>
+        </div>
+    </div>
+</div>
+{{end}}
