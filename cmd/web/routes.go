72aa69a1dc107f6de56e8f415363b6f0bfdbc815
Neni <felipe.dacs@gmail.com>
2023-10-15 14:52:48 -0300

    :tv: add aula 31

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,7 +16,11 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/", app.Home)
 	mux.Post("/login", app.Login)
-	mux.Get("/user/profile", app.Profile)
+
+	mux.Route("/user", func(mux chi.Router) {
+		mux.Use(app.auth)
+		mux.Get("/profile", app.Profile)
+	})
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
