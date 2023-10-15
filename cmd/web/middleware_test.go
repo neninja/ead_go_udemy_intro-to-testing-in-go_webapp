c377a73cba0c6b50f1ded66c56fb1dd03c503af6
Neni <felipe.dacs@gmail.com>
2023-10-15 15:04:03 -0300

    :tv: add aula 32

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"webapp/pkg/data"
 )
 
 func Test_application(t *testing.T) {
@@ -69,5 +70,39 @@ func Test_application_ipFromContext(t *testing.T) {
 	// assert
 	if !strings.EqualFold("whatever", ip) {
 		t.Error("wrong value returned from context")
+	}
+}
+
+func Test_app_auth(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	var tt = []struct {
+		name   string
+		isAuth bool
+	}{
+		{"logged in", true},
+		{"not logged in", false},
+	}
+
+	for _, e := range tt {
+		handlerToTest := app.auth(nextHandler)
+		req := httptest.NewRequest("GET", "http://testing", nil)
+		req = addContextAndSessionToRequest(req, app)
+		if e.isAuth {
+			app.Session.Put(req.Context(), "user", data.User{ID: 1})
+		}
+
+		rr := httptest.NewRecorder()
+		handlerToTest.ServeHTTP(rr, req)
+
+		if e.isAuth && rr.Code != http.StatusOK {
+			t.Errorf("%s: expected status code of 200 but got %d", e.name, rr.Code)
+		}
+
+		if !e.isAuth && rr.Code != http.StatusTemporaryRedirect {
+			t.Errorf("%s: expected status code 307 but got %d", e.name, rr.Code)
+		}
 	}
 }
